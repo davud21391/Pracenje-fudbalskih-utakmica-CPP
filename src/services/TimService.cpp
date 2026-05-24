d13bc9f84d0910b06f0bdd47ebb003652d1697da
DavudFazlic <davud.fazlic@pearshadow.com>
2026-05-24 17:11:11 +0200

    CPP-2 Implement player managment system

--- src/services/TimService.cpp
+++ src/services/TimService.cpp
@@ -3,7 +3,7 @@
 #include <stdexcept>
 
 TimService::TimService(TimRepository& timRepository)
-    : timRepository(timRepository), nextTimId(1) {}
+    : timRepository(timRepository), nextTimId(1), nextIgracId(1) {}
 
 Tim* TimService::dodajTim(const std::string& naziv, const std::string& grad, const std::string& trener, int godinaOsnivanja) {
     validirajTim(naziv, grad, trener, godinaOsnivanja);
@@ -13,12 +13,34 @@ Tim* TimService::dodajTim(const std::string& naziv, const std::string& grad, con
     return tim;
 }
 
+Igrac* TimService::dodajIgracaUTim(int idTima, const std::string& ime, const std::string& prezime, int brojDresa, const std::string& pozicija) {
+    Tim* tim = pronadjiTim(idTima);
+    if (tim == nullptr) {
+        throw std::runtime_error("Tim sa zadanim ID-em ne postoji.");
+    }
+
+    validirajIgraca(*tim, ime, prezime, brojDresa, pozicija);
+
+    Igrac* igrac = new Igrac(nextIgracId++, ime, prezime, brojDresa, pozicija, tim->getIdTima());
+    tim->dodajIgraca(igrac);
+    return igrac;
+}
+
 const std::vector<Tim*>& TimService::vratiSveTimove() const {
     return timRepository.getAll();
 }
 
 Tim* TimService::pronadjiTim(int idTima) const {
     return timRepository.findById(idTima);
+}
+
+const std::vector<Igrac*>& TimService::vratiIgraceTima(int idTima) const {
+    Tim* tim = pronadjiTim(idTima);
+    if (tim == nullptr) {
+        throw std::runtime_error("Tim sa zadanim ID-em ne postoji.");
+    }
+
+    return tim->getIgraci();
 }
 
 void TimService::validirajTim(const std::string& naziv, const std::string& grad, const std::string& trener, int godinaOsnivanja) const {
@@ -40,5 +62,27 @@ void TimService::validirajTim(const std::string& naziv, const std::string& grad,
 
     if (timRepository.findByName(naziv) != nullptr) {
         throw std::runtime_error("Tim sa tim nazivom vec postoji.");
+    }
+}
+
+void TimService::validirajIgraca(const Tim& tim, const std::string& ime, const std::string& prezime, int brojDresa, const std::string& pozicija) const {
+    if (ime.empty()) {
+        throw std::runtime_error("Ime igraca je obavezno.");
+    }
+
+    if (prezime.empty()) {
+        throw std::runtime_error("Prezime igraca je obavezno.");
+    }
+
+    if (pozicija.empty()) {
+        throw std::runtime_error("Pozicija igraca je obavezna.");
+    }
+
+    if (brojDresa < 1 || brojDresa > 99) {
+        throw std::runtime_error("Broj dresa mora biti izmedju 1 i 99.");
+    }
+
+    if (tim.postojiBrojDresa(brojDresa)) {
+        throw std::runtime_error("Igrac sa tim brojem dresa vec postoji u timu.");
     }
 }
